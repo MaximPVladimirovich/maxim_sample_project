db2a5d5e82aac0ab9b8340585562df6b39747726
maxim <maxim.bleeker@gmail.com>
2022-03-02 12:27:02 -0600

    finished styling new builder page

--- client/src/components/NewBuilder.js
+++ client/src/components/NewBuilder.js
@@ -1,9 +1,7 @@
-import { useEffect, useState } from "react"
+import { Paper, Box, List, ListItem, Typography, Grid, TextField, Button } from "@mui/material";
+import { useState } from "react"
 
 export default function NewBuilder() {
-    const [name, setName] = useState('');
-    const [phone, setPhone] = useState('');
-    const [email, setEmail] = useState('');
     const [builder, setBuilder] = useState({
         createdAt: new Date(),
         name: "",
@@ -22,24 +20,41 @@ export default function NewBuilder() {
     const handleCoordinateshange = (e) => {
         setBuilder({ ...builder, coordinates: { ...builder.coordinates, [e.target.name]: e.target.value } })
     }
-    // const handleLatChange = (e) => {
-    //     setAddress({ ...address, coordinates: { ...address.coordinates, lat: e.target.value } })
-    // }
-    // const handleDescriptionChange = (e) => {
-    //     setAddress({ ...address, description: e.target.value })
-    // }
-    // below function will be called when user
-    // click on submit button .
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        sendData()
+        // Validates
+        let validated = Object.keys(builder).every(key => {
+            if (builder[key] === "") {
+                return false;
+            }
+            return true
+        })
+
+        if (validated) {
+            sendData();
+        } else {
+            alert("Please complete the form.")
+            console.log(builder)
+        }
+        setBuilder({
+            createdAt: new Date(),
+            name: "",
+            phone: "",
+            email: "",
+            coordinates: {
+                lon: "",
+                lat: ""
+            },
+            description: ""
+        })
     }
 
+    // Sends to local node server.
     function sendData() {
         const payload = JSON.stringify({
             builder
         })
-        console.log(builder)
 
         const headers = new Headers({
             'Content-Type': 'application/json',
@@ -47,42 +62,109 @@ export default function NewBuilder() {
         })
 
         // Send data to the backend via POST
-        fetch('http://localhost:3001', {  // Enter your IP address here
+        fetch('http://localhost:3001', {
             method: 'POST',
             headers: headers,
             body: payload
         })
     }
     return (
-        <div className="App">
-            <form onSubmit={(e) => { handleSubmit(e) }}>
-                <h2> Create new builder</h2>
-                <label >
-                    Name:
-                </label><br />
-                <input type="text" name="name" value={builder.name} required onChange={(e) => { handleChange(e) }} /><br />
-                <label >
-                    Phone:
-                </label><br />
-                <input type="tel" name="phone" value={builder.phone} required onChange={(e) => { handleChange(e) }} /><br />
-                <label>
-                    Email:
-                </label><br />
-                <input type="email" name="email" value={builder.email} required onChange={(e) => { handleChange(e) }} /><br />
-                <label>
-                    Longitude:
-                </label><br />
-                <input type="number" name="lon" value={builder.coordinates.lon} required onChange={(e) => { handleCoordinateshange(e) }} /><br />
-                <label>
-                    Latitude:
-                </label><br />
-                <input type="number" name="lat" value={builder.coordinates.lat} required onChange={(e) => { handleCoordinateshange(e) }} /><br />
-                <label>
-                    Description:
-                </label><br />
-                <textarea type="text" name="description" value={builder.description} required onChange={(e) => { handleChange(e) }} /><br />
-                <input type="submit" value="Submit" />
-            </form>
-        </div>
+        <Box sx={{ flexGrow: 1 }}>
+            <Paper sx={{ width: 1 / 2, mx: 'auto', my: 5 }}>
+                <List sx={{ mx: 'auto' }}>
+                    <ListItem>
+                        <Typography variant="h4">
+                            Create new Builder
+                        </Typography>
+                    </ListItem>
+                    <ListItem>
+                        <Grid container spacing={4}>
+                            <Grid item>
+                                <TextField
+                                    required
+                                    id="name"
+                                    size="small"
+                                    type="text"
+                                    name="name"
+                                    label="name"
+                                    value={builder.name}
+                                    onChange={e => handleChange(e)}>
+                                    Name:
+                                </TextField>
+                            </Grid>
+                            <Grid item>
+                                <TextField
+                                    required
+                                    id="phone"
+                                    size="small"
+                                    type="tel"
+                                    name="phone"
+                                    label="phone"
+                                    value={builder.phone}
+                                    onChange={e => handleChange(e)}>
+                                    Phone:
+                                </TextField>
+                            </Grid>
+                            <Grid item>
+                                <TextField
+                                    required
+                                    id="email"
+                                    size="small"
+                                    type="text"
+                                    name="email"
+                                    label="email"
+                                    value={builder.email}
+                                    onChange={e => handleChange(e)}>
+                                    Email:
+                                </TextField>
+                            </Grid>
+                            <Grid item>
+                                <TextField
+                                    required
+                                    id="lon"
+                                    size="small"
+                                    type="number"
+                                    name="lon"
+                                    label="Longitude"
+                                    value={builder.coordinates.lon}
+                                    onChange={e => handleCoordinateshange(e)}>
+                                    Longitude:
+                                </TextField>
+                            </Grid>
+                            <Grid item>
+                                <TextField
+                                    required
+                                    id="lat"
+                                    size="small"
+                                    type="number"
+                                    name="lat"
+                                    label="Latitude"
+                                    value={builder.coordinates.lat}
+                                    onChange={e => handleCoordinateshange(e)}>
+                                    Latitude:
+                                </TextField>
+                            </Grid>
+                            <Grid item>
+                                <TextField
+                                    required
+                                    id="description"
+                                    size="small"
+                                    type="text"
+                                    name="description"
+                                    label="Address description"
+                                    value={builder.description}
+                                    onChange={e => handleChange(e)}>
+                                    Description
+                                </TextField>
+                            </Grid>
+                        </Grid>
+                    </ListItem>
+                    <ListItem>
+                        <Button variant="outlined" onClick={(e) => handleSubmit(e)} >Create Builder</Button>
+                    </ListItem>
+                </List>
+            </Paper>
+        </Box>
+
     );
 }
